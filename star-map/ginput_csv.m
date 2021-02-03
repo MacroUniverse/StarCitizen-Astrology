d2a8eb5a26a1db36f3d223ed3396459c1ea0cee3
MacroUniverse <270174408@qq.com>
2021-02-02 19:46:24 -0600

    update

--- star-map/ginput_csv.m
+++ star-map/ginput_csv.m
@@ -1,10 +1,10 @@
 close all;
 path = 'C:\Users\addis\Documents\GitHub\StarCitizen-Astronomy\star-map\';
-imfiles = '3.png';
-figure; imshow(I); hold on;
+imfiles = '7.png';
 I = imread([path imfiles]);
+figure; imshow(I); hold on;
 xy = zeros(100,2);
 for i = 1:100
-    xy(i,:) = ginput(1);
+    xy(i,:) = ginput(1); % break here to adjust image
     disp(xy(i,:));
 end
